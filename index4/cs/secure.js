3a6bec9ec349348999cae02bf3b9d40aa9c824a5
tafengtravel <52737039+tafengtravel@users.noreply.github.com>
2020-09-08 16:38:00 +0800

    Create secure.js

---
+++ index4/cs/secure.js
@@ -0,0 +1,32 @@
+firebase.initializeApp({
+    apiKey:"AIzaSyDYLp1AZezHuoCsdejK-OzHoE9QGn5FzJw",
+    projectId: 'tafengtravel-7cf35',
+    authDomain: "tafengtravel-7cf35.firebaseapp.com",
+    databaseURL: "https://tafengtravel-7cf35.firebaseio.com/",
+  }); 
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+var mail;
+
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("成功");
+    document.getElementById("mail").value = "登入狀態：已登入";
+
+    info(user.email);
+
+  } else {
+    console.log("x");
+    document.getElementById("mail").value = "登入狀態：未登入";
+  }
+});
+
+
+
+
+
+
+
+
+
