4e31539c5e14a1a0a4302f519a96121db7d1b7b2
tafengtravel <52737039+tafengtravel@users.noreply.github.com>
2020-09-08 14:52:19 +0800

    Update secure.js

--- index4/secure.js
+++ index4/secure.js
@@ -14,14 +14,26 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     console.log("成功");
     document.getElementById("mail").value = "登入狀態：已登入";
-
     info(user.email);
+    
+    if (user.email == 'kevin860320@gmail.com' || user.email == 'tafengtravel@gmail.com' || user.email == 'amy0919887@gmail.com'){
+      return 0;
+    }else{
+      alert('並非管理員帳號')
+      firebase.auth().signOut()
+    }
 
   } else {
     console.log("x");
     document.getElementById("mail").value = "登入狀態：未登入";
   }
 });
+
+
+
+
+
+
 
 
 
