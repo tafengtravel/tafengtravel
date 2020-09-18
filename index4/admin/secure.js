5c60ebd2aed68de8aa9212458f38705ab909ab2a
tafengtravel <52737039+tafengtravel@users.noreply.github.com>
2020-09-18 19:16:44 +0800

    Update secure.js

--- index4/admin/secure.js
+++ index4/admin/secure.js
@@ -3,7 +3,6 @@ firebase.initializeApp({
     projectId: 'tafengtravel-7cf35',
     authDomain: "tafengtravel-7cf35.firebaseapp.com",
     databaseURL: "https://tafengtravel-7cf35.firebaseio.com/",
-    storageBucket: "tafengtravel-7cf35.appspot.com",
   }); 
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
 var mail;
@@ -14,16 +13,9 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     console.log("成功");
     document.getElementById("mail").value = "登入狀態：已登入";
-    info(user.email);
-    
-    if (user.uid == 'bnICmkLxO0OTHTbOopiNtWwTKY83' || user.uid == '9my42qdbUFUYqQO4WNykOTgzekY2' || user.uid == 'mPaUjWY6SjfX52nEXjGKQy1XXav2'){
-      console.log(user.uid);
-      return 0;
 
-    }else{
-      alert('並非管理員帳號')
-      firebase.auth().signOut()
-    }
+    info(user.email);
+    oldnumber();
 
   } else {
     console.log("x");
@@ -31,11 +23,18 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 });
 
+function oldnumber(){
 
-
-
-
-
+  let oldnumber = localStorage.getItem("oldnumber");
+  let depdate= localStorage.getItem("depdate");
+  console.log(oldnumber);
+  if (oldnumber != null){
+    document.getElementById('number').value=oldnumber
+    document.getElementById('depdate').value=depdate
+    search();
+  }
+  localStorage.clear()
+}
 
 
 
