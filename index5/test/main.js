503d70379a50984392dd4c02df368bcd4f83ba6d
tafengtravel <52737039+tafengtravel@users.noreply.github.com>
2021-07-06 09:59:27 +0800

    Update main.js

--- index5/test/main.js
+++ index5/test/main.js
@@ -10,7 +10,7 @@ const ChildComponent = {
 
 const router = new VueRouter({
   mode: 'history',
-  base: '/tafengtravel/index5/test/',
+  base: '/index5/test/',
   routes: [
     {
       path: '/',
