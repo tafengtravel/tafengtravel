09bc9043468e349cae15af616018a96e12bfa582
tafengtravel <52737039+tafengtravel@users.noreply.github.com>
2021-07-06 16:58:44 +0800

    Create main.js

---
+++ index6/main.js
@@ -0,0 +1,90 @@
+const ParentComponent = {
+  name: 'parent',
+  template: '<router-view/>'
+}
+
+const ChildComponent = {
+  name: 'child',
+  template: '<h1>Static-Page SPA : {{ $route.params }}</h1>'
+  ,
+  
+}
+
+let id1 = ''
+
+const router = new VueRouter({
+  mode: 'history',
+  base: '/tafengtravel/',
+  routes: [
+    {
+      path: '/index6/',
+      name: 'home',
+      component: {
+        name: 'home',
+        template: '<h1>Static-Page SPA : Home</h1>'
+      },
+    },
+    {
+      path: '/index6/id/:id',
+      component: ParentComponent,
+      children: [
+        {
+          path: '',
+          name: 'foo',
+          component: ChildComponent,
+          meta: {
+            title: 'Foo'
+          }
+        },
+        {
+          path: 'bar',
+          component: ParentComponent,
+          children: [
+            {
+              path: '',
+              name: 'bar',
+              component: ChildComponent,
+              meta: {
+                title: 'Foo / Bar'
+              }
+            },
+            {
+              path: 'baz',
+              name: 'baz',
+              component: ChildComponent,
+              meta: {
+                title: 'Foo / Bar / Baz'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      path: '*',
+      name: 'e404',
+      component: {
+        name: 'e404',
+        template: '<h1>404 Page Not Found!</h1>'
+      }
+    }
+  ]
+})
+
+const $app = new Vue({
+  router,
+  template: '<router-view/>',
+  created () {
+    if (sessionStorage.redirect) {
+      const redirect = sessionStorage.redirect
+      delete sessionStorage.redirect
+      console.log(redirect)
+
+      this.$router.push(redirect)
+    }
+  },mounted(){
+    console.log(this.$route.params)
+  }
+})
+
+$app.$mount('#app')
